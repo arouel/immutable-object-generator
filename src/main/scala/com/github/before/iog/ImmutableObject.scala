862396fc386ff76a37f16e3aaa0e77118924d2ad
André Rouél <andre.rouel@googlemail.com>
2014-05-05 09:05:51 +0200

    Added dedicated functions to define an immutable object as compilation unit that can be rendered afterwards and removed unused generator

---
+++ src/main/scala/com/github/before/iog/ImmutableObject.scala
@@ -0,0 +1,46 @@
+package com.github.before.iog
+
+object ImmutableObject {
+
+  case class Member(
+    val name: String,
+    val `type`: Type,
+    val accessorMethodPrefix: String = "get",
+    val annotations: Seq[Annotation] = Seq())
+
+  def accessorMethodName(member: Member): String = {
+    val first = member.name.head.toUpper
+    val rest = member.name.tail
+    member.accessorMethodPrefix + first + rest
+  }
+
+  def accessorMethods(members: Seq[Member]): Seq[Method] = members.map(memberToAccessor(_))
+
+  def define(pkg: Package, name: String, members: Seq[Member]): CompilationUnit = {
+    val listType = TypeRef.fullyQualifiedName("java.util.List")
+    val imports = typeRefs(members)
+    val fields = this.fields(members)
+    val methods = accessorMethods(members)
+    val immutable = Class(Public, true, name, fields, methods, Seq(), Some(pkg))
+    val types = Seq(immutable)
+    CompilationUnit(Some(pkg), imports, types)
+  }
+
+  def fields(members: Seq[Member]): Seq[Field] = members.map(memberToField(_))
+
+  def memberToAccessor(m: Member): Method = Method(m.annotations, Public, false, false, m.`type`, Seq(), accessorMethodName(m), "return this." + m.name + ";")
+
+  def memberToField(m: Member): Field = Field(m.annotations, Private, false, true, m.`type`, m.name, null)
+
+  def typeRefs(fields: Iterable[Member]): Set[TypeRef] = {
+    val types = fields.map(_.`type`)
+    val empty: Set[TypeRef] = Set()
+    types.foldLeft(empty)((set, t) => t match {
+      case TypeRef(pkg, name, generics) =>
+        if (pkg == Package(Seq("java", "lang"))) set
+        else set + TypeRef(pkg, name, generics)
+      case _ => set
+    })
+  }
+
+}
---
+++ src/test/scala/com/github/before/iog/ImmutableObjectSpec.scala
@@ -0,0 +1,34 @@
+package com.github.before.iog
+
+import org.specs2.mutable._
+import ImmutableObject._
+
+class ImmutableObjectSpec extends Specification {
+
+  "ImmutableObject" should {
+
+    val pkg = Package(Seq("org", "github", "before", "test"))
+    val stringType = TypeRef.fullyQualifiedName("java.lang.String")
+    val listOfStringsType = TypeRef.fullyQualifiedName("java.util.List", Seq(GenericType(stringType)))
+    val imports = Set(listOfStringsType)
+    val numberField = Field(Seq(), Private, false, true, Int, "number")
+    val listField = Field(Seq(), Private, false, true, listOfStringsType, "strings")
+    val fields = Seq(numberField, listField)
+    val numberMethod = Method(Seq(), Public, false, false, Int, Seq(), "getNumber", "return this." + numberField.name + ";")
+    val listMethod = Method(Seq(), Public, false, false, listOfStringsType, Seq(), "getStrings", "return this." + listField.name + ";")
+    val methods = Seq(numberMethod, listMethod)
+    val className = "TestImmutable"
+    val clazz = Class(Public, true, className, fields, methods, Seq(), Some(pkg))
+    val types = Seq(clazz)
+    val compilationUnit = CompilationUnit(Some(pkg), imports, types)
+
+    "define an immutable object" in {
+      val number = Member("number", Int)
+      val strings = Member("strings", TypeRef.fullyQualifiedName("java.util.List", Seq(GenericType(stringType))))
+      val members = Seq(number, strings)
+      define(pkg, className, members) must equalTo(CompilationUnit(Some(pkg), imports, types))
+    }
+
+  }
+
+}
